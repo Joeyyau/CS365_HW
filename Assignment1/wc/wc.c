29eb5fbd8c2a6a313fa74cea11a4d701d7041284
Joey Tavares <jtavares15@mail.wou.edu>
2019-10-13 15:45:07 -0700

    trying to get flags to work

--- Assignment1/wc/wc.c
+++ Assignment1/wc/wc.c
@@ -1,16 +1,31 @@
 #include"stdio.h"
 #include<stdlib.h>
+
+long getFileSize(const char* filename)
+{
+    long size;
+    FILE *f;
+ 
+    f = fopen(filename, "rb");
+    if (f == NULL) return -1;
+    fseek(f, 0, SEEK_END);
+    size = ftell(f);
+    fclose(f);
+	
+    return size;
+}
+
 int main(int argc, char *argv[]) {
 	
 	FILE *fp;
 	char *mode = "r";
 	char outputFilename[] = "out.list";
-	char filename[100];
+	//char filename[100];
 	char ch;
 	
 	//Total Chars, Space, Word counters
 	int tChars = 0;
-	int tSpaces = 0;	
+	int tLines = 0;	
 	int tWords = 0; 
 	
 	int n, last = '\n';
@@ -25,29 +40,46 @@ if(fp){
 		last = n;
 		tChars++;
 		if(ch != ' ' && ch != '\n'){
-			++tChars;
+			++tWords;
 		}
 		if(ch == ' ' || ch == '\n'){
 			++tWords;
 		}
 		if(ch == '\n'){
-			++tSpaces;
+			++tLines;
 		}
 	}
 	if(tChars > 0){
-		++tSpaces;
+		++tLines;
 		++tWords;
 	}	
 }
-
 else{
 	printf("Failed to open the file.\n");
+	return 0;
 }
 
-	printf("Chars: %d \n", tChars);
-    printf("Spaces: %d \n", tSpaces);
-	printf("Words: %d \n", tWords);
+	
 
-//getchar();
+	if(argv[2] = "-c"){
+		printf("Chars: %d \n", tChars);
+		return 0;	
+	}
+    if(argv[2] = "-l"){
+		printf("Lines: %d \n", tLines);
+		return 0;
+	}
+	if(argv[2] = "-w"){
+		printf("Words: %d \n", tWords);
+		return 0;
+	}
+	else{
+		printf("Chars: %d \n", tChars);
+		printf("Lines: %d \n", tLines);
+		printf("Words: %d \n", tWords);
+		return 0;
+}
 return 0;
 }
+
+
